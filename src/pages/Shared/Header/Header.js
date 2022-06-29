d19b90c4280a6e1b94171714021b55e8612efd7c
sa-sabuj <sasabuj321@gmail.com>
2022-06-30 04:41:38 +0600

    services section added & do responsive

---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h2 className='text-center'>This is header</h2>
+        </header>
+    );
+};
+
+export default Header;
